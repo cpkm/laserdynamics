b551bb570304e2b8a34553a8d62d1dd4aa3cd11e
Kyle Air <cpkmanchee@gmail.com>
2016-11-12 16:31:54 -0500

    created pusletestscript.py

--- pulsemodelling/pulsemodel.py
+++ pulsemodelling/pulsemodel.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jul 11 15:09:06 2014
+Created on Wed Oct 19 11:02:23 2016
 
 @author: cpkmanchee
 
---
+++ pulsemodelling/pulsetestscript.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 16:25:06 2014
+
+@author: cpkmanchee
+
+Notes:
+This file requires pulsemodel.py to be run previously
+This file uses the functions and classes defined in pulsemodel.py
+
+"""
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+import sys
+import inspect
+
+#constants
+h = 6.62606957E-34  #J*s
+c = 299792458.0     #m/s
